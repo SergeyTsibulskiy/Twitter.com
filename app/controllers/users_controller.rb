c8f09238df29e2b05725726b6bfc3a4c027086f6
SergeyTsibulskiy <c4r0n0s@gmail.com>
2014-09-01 17:56:52 +0300

    styling(+-) nav panel  when user authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def profile
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :fullname) }
+  end
 end
---
+++ db/migrate/20140901074048_add_user_full_name_to_registration.rb
@@ -0,0 +1,5 @@
+class AddUserFullNameToRegistration < ActiveRecord::Migration
+  def change
+    add_column(:users, :fullname, :string)
+  end
+end
